a0488a2a037b971aea04b353dfe73accc685745a
Gordeyev <Aleksey.Gordeyev@METRAN.local>
2022-08-31 08:51:32 +0500

    Finished with VK part

--- VK_photo_saver.py
+++ VK_photo_saver.py
@@ -3,7 +3,7 @@ import requests
 from pprint import pprint
 
 
-class VKPhotoSaver:
+class VKtoYaDiskPhotoSaver:
     
     def __init__(self, VK_id, ya_token = None) -> None:
         self.ya_token = ya_token
@@ -15,23 +15,31 @@ class VKPhotoSaver:
             'access_token' : self.VK_token,
             'v': '5.131'
         }
-    
-   
+       
     def get_VK_photo_info(self):
         self.VK_method = ('photos.get')
         get_VK_photo_info_params = {
             'owner_id': self.VK_id,
             'album_id': 'profile',
+            'extended': '1'
         }
         req = requests.get(self.url + self.VK_method, params={**self.params, **get_VK_photo_info_params})
         return req.json()['response']['items']
+#Input: class attrubutes (self.VK_id)
+#Output: photos from VK profile info pulled from response -> items (extended = 1)
+    
+    def get_max_VK_photos(self, response_items):
+        result = {}
+        max_photo = 0
+        for photo in response_items:
+            for max_size_photo in photo['sizes']:
+                if max_size_photo['height'] * max_size_photo['width'] < max_photo:
+                    result.setdefault(photo['likes']['count'],[max_size_photo['url'], max_size_photo['type']])
+                else:
+                    max_photo = max_size_photo['height'] * max_size_photo['width']
+        return result
+#Input: get_VK_photo_info result
+#Output: dict ({likes_count:[url, type]})
 
-my_app = VKPhotoSaver('1')
-photos = my_app.get_VK_photo_info()
-# pprint(photos[0]['sizes'])
-result = []
-for photo in photos:
-    for max_size_photo in photo['sizes']:
-        if max_size_photo['type'] == 'z':
-            result.append(max_size_photo)
-pprint(result)
+my_app = VKtoYaDiskPhotoSaver('1')
+pprint(my_app.get_max_VK_photos(my_app.get_VK_photo_info()))
